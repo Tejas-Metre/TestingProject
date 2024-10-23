54ba0c82f73e89540543b62417085d59aaecb2d2
Tejas-Metre <tejasametre1@gmail.com>
2024-10-23 13:14:05 +0530

    seventh commit

---
+++ src/test/java/demo/PracticeProgram/DemoProgram.java
@@ -0,0 +1,8 @@
+
+package demo.PracticeProgram;
+
+public class DemoProgram {
+public static void main(String[] args) {
+	System.out.println("Hello words");
+}
+}
--- src/test/java/demo/PracticeProgram/PushFile.java
+++
@@ -1,9 +0,0 @@
-package demo.PracticeProgram;
-
-public class PushFile {
-	public static void main(String[] args) {
-		System.out.println("Hello Java");
-		
-	}
-
-}
