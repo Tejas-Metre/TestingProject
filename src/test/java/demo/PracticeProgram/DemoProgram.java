25021da06799a0ed47cd9b23ee2c0b47ae5acaae
Tejas-Metre <tejasametre1@gmail.com>
2024-10-23 15:27:21 +0530

    ninth commit

--- src/test/java/demo/PracticeProgram/DemoProgram.java
+++ src/test/java/demo/PracticeProgram/DemoProgram.java
@@ -5,5 +5,6 @@ public class DemoProgram {
 public static void main(String[] args) {
 	System.out.println("Hello words");
 	System.out.println("Hello words2");
+	System.out.println("Hello words3");
 }
 }
